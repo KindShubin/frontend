a111bf1e5fabc93c9351e72c19987c8e69aac5fe
Dmitriy <anoshindmitriy@gmail.com>
2023-03-14 22:26:19 +0200

    delete old trash

--- src/file.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-import {sumSquareDifference} from './script.js';
-
-console.log("begin file!");
-const a = sumSquareDifference(8);
-console.log(a);
-console.log("end file!");
--- src/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import * as script from './script.js';
-
-console.log("begin test");
-console.log(script);
-const a = script.sumSquareDifference(6);
-console.log(a);
-console.log("end test");
